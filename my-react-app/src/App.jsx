f8a5e4acda2d7b76617a19b392ed077753428e8d
BalandinaM <marina.balandina.89@mail.ru>
2025-01-19 23:18:51 +0500

    Добавлены новые пути роута

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,6 +6,8 @@ import Login from './Components/Login/Login';
 import GlobalStyles from './GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import theme from './assests/constants/theme';
+import SignUp from './Components/SignUp/SignUp';
+import ListCheckLists from './Components/ListCheckLists/ListCheckLists';
 // import Button from '@mui/material/Button';
 // import Container from '@mui/material/Container';
 
@@ -20,6 +22,8 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/login" element={<Login />} />
+					<Route path='/signup'element={<SignUp />}/>
+					<Route path='/list' element={<ListCheckLists />}/>
 				</Routes>
 			</ThemeProvider>
 		</HashRouter>
