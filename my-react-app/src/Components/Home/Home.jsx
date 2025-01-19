57e62b66088c2e4d20a892760e278212dd668126
BalandinaM <marina.balandina.89@mail.ru>
2025-01-19 23:18:23 +0500

    Домашняя страница

--- my-react-app/src/Components/Home/Home.jsx
+++ my-react-app/src/Components/Home/Home.jsx
@@ -1,22 +1,55 @@
-import {useState, useEffect} from "react";
+import {useEffect} from "react";
 import { useSelector } from 'react-redux';
-import Login from "../Login/Login";
+import { NavLink, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const TitleGreeting = styled.p`
+	font-size: 7.2rem;
+	font-weight: 700;
+`;
+
+const TextGreeting = styled.p`
+	font-size: 5.2rem;
+	font-weight: 500;
+	margin-bottom: 3.2rem;
+`;
+
+const WrapLink = styled.div`
+	display: flex;
+	width: 500px;
+	justify-content: space-between;
+	align-items: center;
+	margin: 0 auto;
+`;
+
+const NavLinkCustom = styled(NavLink)`
+	font-size: 2.4rem;
+	padding: 10px 40px;
+	border-radius: 5%;
+	box-shadow: 0px 5px 10px 2px rgba(0, 62, 100, 0.2);
+`
 
 const Home = () => {
-	const [isAuth, setIsAuth] = useState(false);
 	const authToken = useSelector((state) => state.auth.isAuth);
+	const navigate = useNavigate()
 
 	useEffect(() => {
 			if (authToken) {
-				setIsAuth(true);
+				navigate('/list')
 			}
-		}, [authToken]);
+		}, [authToken, navigate]);
 
-	return isAuth ? (
-			<p>Список чеклистов</p>
-	) : (
-			<Login/>
+	return (
+		<>
+			<TitleGreeting>Добро пожаловать!</TitleGreeting>
+			<TextGreeting>Наведите порядок в списках своих дел, используя наше простое и лаконичное решение</TextGreeting>
+			<WrapLink>
+				<NavLinkCustom to='/signup'>Регистрация</NavLinkCustom>
+				<NavLinkCustom to='/login'>Авторизация</NavLinkCustom>
+			</WrapLink>
+		</>
 	)
+
 }
 
 export default Home;
