047cddb222c54caa781d7e3dbae37d7942c18ee1
BalandinaM <marina.balandina.89@mail.ru>
2025-01-20 23:42:13 +0500

    Обертка для блоков

---
+++ my-react-app/src/Elements/Wrapper.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+	flex-grow: 1;
+	width: 980px;
+	margin: 0 auto;
+	padding: 30px 60px;
+	/* width: ${props => props.$width || '1120px'};
+
+	padding: 50px 50px;
+	text-align: ${props => props.$textalign || 'center'};
+	background-color: ${props => props.$bg || null};
+	position: relative; */
+`;
