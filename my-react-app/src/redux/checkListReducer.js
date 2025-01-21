a4a8568611c3b739f5ed6029e43607bc98f3376f
BalandinaM <marina.balandina.89@mail.ru>
2025-01-21 22:53:45 +0500

    Добавила еще записи в данные

--- my-react-app/src/redux/checkListReducer.js
+++ my-react-app/src/redux/checkListReducer.js
@@ -120,6 +120,294 @@ const initialState = {
 				},
 			],
 		},
+		{
+			id: "5",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "51",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "52",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "53",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "54",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "55",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
+		{
+			id: "6",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "61",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "62",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "63",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "64",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "65",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
+		{
+			id: "7",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "71",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "72",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "73",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "74",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "75",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
+		{
+			id: "8",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "81",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "82",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "83",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "84",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "85",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
+		{
+			id: "9",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "91",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "92",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "93",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "94",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "95",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
+		{
+			id: "10",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "101",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "102",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "103",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "104",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "105",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
+		{
+			id: "11",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "111",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "112",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "113",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "114",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "115",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
+		{
+			id: "12",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "121",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "122",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "123",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "124",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "125",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
+		{
+			id: "13",
+			title: "Прочее важное перед отъездом",
+			description: "По мелочи, но ОЧЕНЬ важно",
+			tasksData: [
+				{
+					idTask: "131",
+					task: "Проверить аптечку",
+					isDone: false,
+				},
+				{
+					idTask: "132",
+					task: "Забронировать транспорт до аэропорта",
+					isDone: false,
+				},
+				{
+					idTask: "133",
+					task: "Зарядка устройств",
+					isDone: false,
+				},
+				{
+					idTask: "134",
+					task: "Позвонить маме",
+					isDone: false,
+				},
+				{
+					idTask: "135",
+					task: "Проверить прогноз погоды",
+					isDone: false,
+				},
+			],
+		},
 	],
 };
 
