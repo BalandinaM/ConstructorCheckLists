c7c30a5046c99cee8cadc6f08781088eb3ad1b44
BalandinaM <marina.balandina.89@mail.ru>
2025-01-21 14:22:06 +0500

    Наброски структуры списка чеклистов

--- my-react-app/src/Components/ListCheckLists/ListCheckLists.jsx
+++ my-react-app/src/Components/ListCheckLists/ListCheckLists.jsx
@@ -3,33 +3,63 @@ import { Wrapper } from "../../Elements/Wrapper";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
+const ListCheckListsWrapper = styled(Wrapper)`
+	display: flex;
+    flex-direction: column;
+`
+
+const Title = styled.h1`
+	margin-bottom: 2.0rem;
+`
+
 const List = styled.ul`
 	display: grid;
-	grid-template-columns: repeat(3, 1fr);
+	grid-template-columns: repeat(4, 1fr);
 	gap: 30px;
 	justify-items: center;
+	margin-bottom: 2.0rem;
 `
 
 const Item = styled.li`
-	width: 250px;
+	width: 100%;
 	border: 2px solid blue;
-  height: 250px;
+  	//height: 250px;
+	aspect-ratio: 1 / 1;
 `;
 
 const NavLinkCustom = styled(NavLink)`
 	display: block;
 	width: 100%;
 	height: 100%;
+`;
+
+const Pagination = styled.ul`
+	display: flex;
+	flex-direction: row;
+	gap: 5px;
+	align-self: center;
 `
+
+const ItemPagination = styled.li`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 30px;
+	height: 30px;
+	border: 2px solid yellow;
+`;
 
 const ListCheckLists= () => {
 	const arrCheckLists = useSelector((state) => state.checklist.checkListsData);
 	console.log(arrCheckLists);
 
 	return (
-		<Wrapper>
-			<h1>Список чеклистов</h1>
+		<ListCheckListsWrapper>
+			<Title>Список чеклистов</Title>
 			<List>
+				<Item>
+					<p>Сюда надо добавить сслыку на создание нового чеклиста</p>
+				</Item>
 				<Item>
 					<NavLinkCustom to="./checklist/${id}">
 						<h3>Заголовок чеклиста</h3>
@@ -37,7 +67,19 @@ const ListCheckLists= () => {
 					</NavLinkCustom>
 				</Item>
 				<Item>
-					Чеклист №2
+					Чеклист №3
+				</Item>
+				<Item>
+					Чеклист №3
+				</Item>
+				<Item>
+					Чеклист №3
+				</Item>
+				<Item>
+					Чеклист №3
+				</Item>
+				<Item>
+					Чеклист №3
 				</Item>
 				<Item>
 					Чеклист №3
@@ -55,7 +97,13 @@ const ListCheckLists= () => {
 					Чеклист №3
 				</Item>
 			</List>
-		</Wrapper>
+			<Pagination>
+				<ItemPagination>1</ItemPagination>
+				<ItemPagination>2</ItemPagination>
+				<ItemPagination>3</ItemPagination>
+				<ItemPagination>4</ItemPagination>
+			</Pagination>
+		</ListCheckListsWrapper>
 	)
 }
 
