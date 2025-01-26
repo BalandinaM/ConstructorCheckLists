6925b5eab3127774181c6a6d397aa46a9102d208
BalandinaM <marina.balandina.89@mail.ru>
2025-01-26 14:42:29 +0500

    добавлено диалоговое окно с добалением элемента списка

--- my-react-app/src/Components/NewCheckList/NewChecklist.jsx
+++ my-react-app/src/Components/NewCheckList/NewChecklist.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { Wrap } from "../../Elements/Wrapper";
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -12,6 +13,7 @@ const WrapForm = styled.div`
 	margin: 0 auto;
 	padding: 30px;
 	border-radius: 2%;
+	position: relative;
 `;
 
 const FormLogin = styled(Form)`
@@ -60,7 +62,65 @@ const ButtonSubmit = styled.button`
 	margin-top: auto;
 `;
 
+const ButtonAddItem = styled(ButtonSubmit)`
+	padding: 15px 25px;
+	background-color: ${(props) => props.theme.colors.primary};
+	font-size: 1.8rem;
+	color: ${(props) => props.theme.colors.primarySecondary};
+	border-radius: 5px;
+	align-self: flex-end;
+	width: fit-content;
+	margin-top: 30px;
+`;
+
+const AddItemBox = styled.div`
+	//display: ${(props) => (props.$isShowConfirm === "true" ? "block" : "none")};
+	display: block;
+	width: 600px;
+	height: auto;
+	padding: 30px;
+	background-color: ${(props) => props.theme.colors.background};
+	box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+	border-radius: 5px;
+	z-index: 4;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+`;
+
+const AddItemBoxWrap = styled.div`
+	position: relative;
+`
+
+const TitleAddItem = styled.h4`
+	font-size: 2.2rem;
+`;
+
+const ButtonSaveItem = styled(ButtonSubmit)`
+	margin-top: 30px;
+`;
+
+const ButtonClose = styled(ButtonSubmit)`
+	background-color: ${(props) => props.theme.colors.background};
+	position: absolute;
+	padding: 0;
+	top: 0;
+	right: 0;
+`
+
 const NewCheckList = () => {
+	const [isShowDialog, setIsShowDialog] = useState(false);
+
+	const handleAddItem = () => {
+		setIsShowDialog(true);
+		console.log(isShowDialog);
+	}
+
+	const handleCloseDialog = () => {
+		setIsShowDialog(false)
+	}
+
 	return (
 		<Wrap>
 			<WrapForm>
@@ -117,6 +177,22 @@ const NewCheckList = () => {
 								/>
 								<ErrorMessageBox name="description" component="div" />
 							</InputWrap>
+							<ButtonAddItem type="button" onClick={handleAddItem}>Добавить элемент списка</ButtonAddItem>
+							{isShowDialog ? (
+								<AddItemBox>
+									<AddItemBoxWrap>
+										<TitleAddItem>Добавить элемент</TitleAddItem>
+										<InputWrap>
+											<LabelTextField htmlFor="newItem">Название</LabelTextField>
+											<TextField type="text" name="newItem"></TextField>
+										</InputWrap>
+										<ButtonSaveItem>Сохранить</ButtonSaveItem>
+										<ButtonClose onClick={handleCloseDialog}>
+											<img src="./image/icon_close.png" alt="Закрыть" width={30} height={30} />
+										</ButtonClose>
+									</AddItemBoxWrap>
+								</AddItemBox>
+							) : null}
 							<ButtonSubmit type="submit" disabled={isSubmitting}>
 								Сохранить чек-лист
 							</ButtonSubmit>
